84b16780f0db7428393eaeff1b9dac51d4d9e2e8
marcoramires <marcoramires@hotmail.com>
2014-07-19 15:55:49 +1000

    Added some fadei-out page transition

--- js/Scripts.js
+++ js/Scripts.js
@@ -1,10 +1,23 @@
 $(document).ready(function () {
+    $("body").css("display", "none");
+
     var src = $('.page-image').attr('src');
     $("#HomeBackground").backstretch(src);
     $("#Side").backstretch(src);
 
     var $window = $(window);
     var $image = $('.backstretch');
+
+    $("body").fadeIn(1000);
+
+    function redirectPage(linkLocation) {
+        window.location = linkLocation;
+    }
+    $("a").click(function(event){
+        event.preventDefault();
+        var linkLocation = this.href;
+        $("body").fadeOut(1000, redirectPage(linkLocation));
+    });
 
     $window.on('scroll', function () {
         var top = $window.scrollTop();
