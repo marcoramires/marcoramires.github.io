f4764d98820cb261478fb921763f45ba5fb859dd
Marco Ramires <marco.ramires@asic.gov.au>
2017-01-06 13:01:04 +1100

    Added theme dependencies

--- new-site/Gulpfile.js
+++ new-site/Gulpfile.js
@@ -10,7 +10,7 @@ var concat = require('gulp-concat');
 var bourbon = require('node-bourbon').includePaths;
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'js', 'js-vendor', 'js-preload'], function() {
+gulp.task('serve', ['sass', 'styles-vendor', 'styles-plugins', 'js', 'js-plugins', 'js-vendor', 'js-preload'], function() {
     browserSync.init({
         server: ['./', '.tmp']
     });
@@ -21,6 +21,7 @@ gulp.task('serve', ['sass', 'js', 'js-vendor', 'js-preload'], function() {
     // add browserSync.reload to the tasks array to make
     // all browsers reload after tasks are complete.
     gulp.watch("app/js/**/*.js", ['js-watch']);
+    gulp.watch("app/styles/**/*.css", ['styles-vendor', 'styles-plugins']);
 });
 
 // create a task that ensures the `js` task is complete before
@@ -32,7 +33,7 @@ gulp.task('js-watch', ['js'], function (done) {
 
 // process JS files and return the stream.
 gulp.task('js', function () {
-    return gulp.src(['app/js/**/*.js', '!app/js/vendor/*.js'])
+    return gulp.src(['app/js/**/*.js', '!app/js/vendor/*.js', '!app/js/plugins/*.js'])
         .pipe(concat('main.js'))
         .pipe(gulp.dest('.tmp/scripts'));
 });
@@ -45,7 +46,12 @@ gulp.task('js-preload', function () {
         .pipe(gulp.dest('.tmp/scripts'));
 });
 
-// process JS files and return the stream.
+gulp.task('js-plugins', function () {
+    return gulp.src(['app/js/plugins/**/*.js'])
+        .pipe(concat('plugins.js'))
+        .pipe(gulp.dest('.tmp/scripts'));
+});
+
 gulp.task('js-vendor', function () {
     return gulp.src(['node_modules/jquery/dist/jquery.js', 'app/js/vendor/vendor.js'])
         .pipe(concat('vendor.js'))
@@ -63,6 +69,18 @@ gulp.task('sass', function() {
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('.tmp/styles/'))
         .pipe(browserSync.stream());
+});
+
+gulp.task('styles-vendor', function() {
+    return gulp.src("app/styles/vendor/*.css")
+        .pipe(concat('vendor.css'))
+       .pipe(gulp.dest('.tmp/styles/'));
+});
+
+gulp.task('styles-plugins', function() {
+    return gulp.src("app/styles/plugins/*.css")
+        .pipe(concat('plugins.css'))
+        .pipe(gulp.dest('.tmp/styles/'));
 });
 
 gulp.task('default', ['serve']);
