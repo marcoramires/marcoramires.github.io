65e4c43fa1cb9ed0a06721cc1fe1f414b7767b28
Marco Ramires <marco.ramires@asic.gov.au>
2017-01-19 14:00:23 +1100

    Added review section

--- new-site/app/layout/picture.controller.js
+++ new-site/app/layout/picture.controller.js
@@ -12,7 +12,7 @@ function PictureController($rootScope, $scope, $log, $stateParams, $state) {
     $log.log('> Picture Name: ', $stateParams.pictureName);
 
     this.details = {
-        name:  "yellow fin",
+        name: "yellow fin",
         location: "Byron Bay",
         dateTaken: "15/06/2016",
         camera: '1/1000 F1.4 ISO100',
@@ -20,6 +20,7 @@ function PictureController($rootScope, $scope, $log, $stateParams, $state) {
     };
 
     $scope.data = {
+        picture: this.details,
         availableOptions: [
             {id: '0', name: '12 x 8 inches - 30 x 20 cm', price: 25},
             {id: '1', name: '18 x 12 inches - 45 x 30 cm', price: 50},
@@ -30,11 +31,76 @@ function PictureController($rootScope, $scope, $log, $stateParams, $state) {
         ]
     };
 
+    //TODO: Move to payment service
+    function $$payPal() {
+        var _env = 'sandbox';
+        var _client = {
+            sandbox: 'AX-MNu6chPspfWTp__Cb2JCpy9Sj9P2NTqC2sO_-j-Gajj_2R6ByPpT2-dMMp0FOZ2d25HJqwfdx4DhB',
+            production: 'xxxxxxxxx'
+        };
+        var _prodDescription = 'Print only:' + $scope.data.picture.name + ' ' + $scope.data.availableOptions[$scope.data.size].name;
+        var _prodValue = $scope.data.availableOptions[$scope.data.size].price;
+        var _total = _prodValue;
 
+        return paypal.Button.render({
+            env: 'sandbox', // Specify 'sandbox' for the test environment
+            client: {
+                sandbox: 'AX-MNu6chPspfWTp__Cb2JCpy9Sj9P2NTqC2sO_-j-Gajj_2R6ByPpT2-dMMp0FOZ2d25HJqwfdx4DhB',
+                production: 'xxxxxxxxx'
+            },
+            payment: function () {
+                return paypal.rest.payment.create(_env, _client, {
+                    intent: "sale",
+                    payer: {
+                        payment_method: "paypal"
+                    },
+                    transactions: [
+                        {
+                            amount: {
+                                total: _total,
+                                currency: 'AUD'
+                            },
+                            item_list: {
+                                items: [
+                                    {
+                                        quantity: "1",
+                                        name: _prodDescription,
+                                        price: _prodValue,
+                                        currency: "AUD",
+                                        description: _prodDescription,
+                                        tax: "0"
+                                    }
+                                ]
+                            },
+                            description: _prodDescription
+                            // custom: "EBAY_EMS_90048630024435",
+                            // invoice_number: "48787589673",
+                        }
+                    ]
+                });
+            },
+            commit: true, // Optional: show a 'Pay Now' button in the checkout flow
+            onAuthorize: function (data, actions) {
+                // Optional: display a confirmation page here
+                return actions.payment.execute().then(function () {
+                    // Show a success page to the buyer
+                    alert('Paypal Success!');
+                });
+            },
+            onCancel: function (data, actions) {
+                return actions.redirect();
+            },
+            style: {
+                size: 'medium',
+                color: 'blue',
+                shape: 'rect'
+            }
+        }, '#paypal-button');
+    }
 
     var _pictureName = $stateParams.pictureName;
-    if(_pictureName.replace('-', ' ').toLowerCase() !== this.details.name) {
-        $state.go('home',  {location: 'replace'})
+    if (_pictureName.replace('-', ' ').toLowerCase() !== this.details.name) {
+        $state.go('home', {location: 'replace'})
     }
 
     /* ----------------------------------------------
@@ -43,65 +109,10 @@ function PictureController($rootScope, $scope, $log, $stateParams, $state) {
     function preloader() {
         Pace.on('done', function () {
             $(".animate-content").addClass('load-finish');
-            if($rootScope.paypal === undefined) {
-                $rootScope.paypal = paypal.Button.render({
-                    env: 'sandbox', // Specify 'sandbox' for the test environment
-                    client: {
-                        sandbox:    'AX-MNu6chPspfWTp__Cb2JCpy9Sj9P2NTqC2sO_-j-Gajj_2R6ByPpT2-dMMp0FOZ2d25HJqwfdx4DhB',
-                        production: 'xxxxxxxxx'
-                    },
-                    payment: function() {
-                        var env = this.props.env;
-                        var client = this.props.client;
-                        return paypal.rest.payment.create(env, client, {
-                            intent: "sale",
-                            payer: {
-                                payment_method: "paypal"
-                            },
-                            transactions: [
-                                {
-                                    amount: {
-                                        total: "1.00",
-                                        currency: "AUD"
-                                    },
-                                    item_list: {
-                                        items: [
-                                            {
-                                                quantity: "1",
-                                                name: "Yellow Fin",
-                                                price: "1",
-                                                currency: "AUD",
-                                                description: "",
-                                                tax: "0"
-                                            }]
-                                    },
-                                    description: "Yellow Fin: Print Only 12x18 (30x45cm) Unframed Print",
-                                    invoice_number: "1234567890",
-                                    custom: "Custom message"
-                                }
-                            ]
-                        });
-                    },
-                    commit: true, // Optional: show a 'Pay Now' button in the checkout flow
-                    onAuthorize: function(data, actions) {
-                        // Optional: display a confirmation page here
-                        return actions.payment.execute().then(function() {
-                            // Show a success page to the buyer
-                        });
-                    },
-                    onCancel: function(data, actions) {
-                        return actions.redirect();
-                    },
-                    style: {
-                        size: 'medium',
-                        color: 'blue',
-                        shape: 'rect'
-                    }
-                }, '#paypal-button');
-            }
             $log.log('* Pre-Loader Done *');
         });
     }
+
     /*----------------------------------------------
      N A V I G A T I O N - TODO: Move to Service layer
      ------------------------------------------------*/
@@ -122,10 +133,23 @@ function PictureController($rootScope, $scope, $log, $stateParams, $state) {
     /*----------------------------------------------
      P A G E S - H E L P E R
      ------------------------------------------------*/
+    function closeLayer() {
+        var $layerPage = $('.layer-page');
+        $layerPage.css({
+            '-webkit-transform': 'none',
+            '-moz-transform': 'none',
+            '-ms-transform': 'none',
+            '-o-transform': 'none',
+            'transform': 'none'
+        });
+        $layerPage.removeClass('active');
+    }
+
     function closeNavVertical() {
         $('.left-menu .navbar').removeClass('active');
         $('#navbar').collapse('hide');
     }
+
     function resizeLayers() {
         var wheight = $(window).height();
         $('.layer-page.active').css({
@@ -136,6 +160,7 @@ function PictureController($rootScope, $scope, $log, $stateParams, $state) {
             'transform': 'matrix(1, 0, 0, 1, 0, -' + wheight + ')'
         });
     }
+
     $(window).on('resize', function (e) {
         resizeLayers();
         closeNavVertical();
@@ -143,11 +168,38 @@ function PictureController($rootScope, $scope, $log, $stateParams, $state) {
     });
 
     /*----------------------------------------------
+     M A N A G E  P A G E S
+     ------------------------------------------------*/
+    function managePages() {
+        $('.open-layer').on('click', function (e) {
+            closeLayer();
+            var wheight = $(window).height();
+            var layerToOpen = $(this).data('layer');
+            $('.layer-page').removeClass('active');
+            $('.' + layerToOpen).css({
+                '-webkit-transform': 'matrix(1, 0, 0, 1, 0, -' + wheight + ')',
+                '-moz-transform': 'matrix(1, 0, 0, 1, 0, -' + wheight + ')',
+                '-ms-transform': 'matrix(1, 0, 0, 1, 0, -' + wheight + ')',
+                '-o-transform': 'matrix(1, 0, 0, 1, 0, -' + wheight + ')',
+                'transform': 'matrix(1, 0, 0, 1, 0, -' + wheight + ')'
+            }).addClass('active');
+            closeNavVertical();
+            console.log(layerToOpen);
+            return false;
+        });
+        $('.close-layer').on('click', function (e) {
+            closeLayer();
+        });
+        $log.log('* Manage-Page Done *');
+    }
+
+    /*----------------------------------------------
      A N G U L A R  R E A D Y
      ------------------------------------------------*/
-    angular.element(document).ready(function() {
+    angular.element(document).ready(function () {
         $('body').addClass('page-blog page-picture');
         preloader();
         navigation();
+        managePages();
     });
 }
